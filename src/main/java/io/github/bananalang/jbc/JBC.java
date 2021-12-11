e68f39ce619422af3ec868c58ac8905cfa76e627
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-12-11 16:39:24 -0600

    It works!

--- src/main/java/io/github/bananalang/jbc/JBC.java
+++ src/main/java/io/github/bananalang/jbc/JBC.java
@@ -1,7 +1,36 @@
 package io.github.bananalang.jbc;
 
+import java.io.File;
+import java.io.IOException;
+
+import io.github.bananalang.JavaBananaConstants;
+import io.github.bananalang.bytecode.ByteCodeFile;
+import io.github.bananalang.compile.BananaCompiler;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 public class JBC {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        ArgumentParser parser = ArgumentParsers.newFor("jbc").build()
+            .defaultHelp(true)
+            .description("JavaBanana compiler");
+        parser.addArgument("file").nargs("+")
+              .help("The file(s) to compile")
+              .type(File.class);
+        Namespace ns = parser.parseArgsOrFail(args);
+        for (File file : ns.<File>getList("file")) {
+            String moduleName = file.getName();
+            moduleName = moduleName.substring(0, moduleName.lastIndexOf('.'));
+            if (JavaBananaConstants.DEBUG) {
+                System.err.println("Compiling " + moduleName);
+            }
+            ByteCodeFile bbc = BananaCompiler.compileFile(file);
+            if (JavaBananaConstants.DEBUG) {
+                System.err.println("Finished compiling " + moduleName);
+                bbc.disassemble(System.err);
+            }
+            bbc.write(new File(file.getParentFile(), moduleName + ".bbc"));
+        }
     }
 }
